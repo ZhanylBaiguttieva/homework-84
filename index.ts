604a8ad85ba345bca89eb2bc85eecb60d8076e9f
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-19 04:40:11 +0600

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import * as mongoose from "mongoose";
+import config from "./config";
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'));
+app.use(express.json());
+const run = async () => {
+    await mongoose.connect(config.mongoose.db);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    })
+};
+
+void run();
