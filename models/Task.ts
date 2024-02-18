882c9bd4736bb4aa49e708e2995d41efebd57760
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-19 05:12:59 +0600

    Added models Task.ts and User.ts

---
+++ models/Task.ts
@@ -0,0 +1,32 @@
+
+import mongoose, {Schema, model, Types} from 'mongoose';
+import User from "./User";
+
+const TaskSchema = new mongoose.Schema({
+   user: {
+       type: Schema.Types.ObjectId,
+       ref: 'User',
+       required: true,
+       validate: {
+           validator: async (value: Types.ObjectId) => {
+               const user = await User.findById(value);
+               return Boolean(user);
+           },
+           message: 'User does not exist!',
+       }
+   },
+   title: {
+       type: String,
+       required: true,
+   },
+   description: {
+       type: String,
+   },
+   status: {
+       type: String,
+       enum : ['new','in_progress','complete']  ,
+   }
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+export default Task;
